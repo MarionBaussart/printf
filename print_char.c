03a2e60350f9b868472ae87834be9781849c3cb9
MarionBaussart <marion.baussart@gmail.com>
2021-11-08 13:00:04 +0000

    T0 - little functions

---
+++ print_char.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdarg.h>
+#include <stddef.h>
+
+/**
+ * print_char - print a char
+ * @print: thing to print
+ * Return: No return.
+ */
+
+void print_char(va_list print)
+{
+	char c = va_arg(print, int);
+
+	_putchar(c);
+}

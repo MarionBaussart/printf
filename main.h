3e9f26837911afade5b61a96be5b7f75ea431f89
MarionBaussart <marion.baussart@gmail.com>
2021-11-08 12:58:50 +0000

    update main.h

--- main.h
+++ main.h
@@ -3,6 +3,23 @@
 #include <stddef.h>
 #include <stdarg.h>
 
+int _putchar(char c);
 int _printf(const char *format, ...);
+
+/**
+ * struct list_format - Struct list_format
+ *
+ * @tf: type of format
+ * @nfunc: name of the function to use
+ */
+typedef struct list_format
+{
+	char *tf;
+	void (*nfunc)(va_list);
+} format_t;
+
+void print_char(va_list print);
+void print_string(va_list print);
+void print_percent(va_list print);
 
 #endif

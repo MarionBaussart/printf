55ffab64524580a4de63605394ff80b01ff12108
AlpagaSauvage <3645@holbertonschool.com>
2021-11-09 10:18:21 +0000

    betty style

--- print_unsigned_number.c
+++ print_unsigned_number.c
@@ -1,35 +1,35 @@
 #include "main.h"
 
 /**
- * print_number - print integer
+ * print_unsigned_number - print integer
  * @n: nombre int
  */
 void print_unsigned_number(unsigned int n)
 {
-    unsigned int nbc = 0;
-    unsigned int num = n;
-    unsigned int i = 1;
+	unsigned int nbc = 0;
+	unsigned int num = n;
+	unsigned int i = 1;
 
-    if (n == 0)
-    {
-        _putchar(48);
-    }
-    else
-    {
-        while (num > 0)
-        {
-            num = num / 10;
-            nbc++;
-        }
-        while (nbc > 1)
-        {
-            i = i * 10;
-            nbc--;
-        }
-        while (i > 0)
-        {
-            _putchar(((n / i) % 10) + 48);
-            i = i / 10;
-        }
-    }
+	if (n == 0)
+	{
+		_putchar(48);
+	}
+	else
+	{
+		while (num > 0)
+		{
+			num = num / 10;
+			nbc++;
+		}
+		while (nbc > 1)
+		{
+			i = i * 10;
+			nbc--;
+		}
+		while (i > 0)
+		{
+			_putchar(((n / i) % 10) + 48);
+			i = i / 10;
+		}
+	}
 }
